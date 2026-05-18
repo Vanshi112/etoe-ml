accf58b3ce79cf8d10be9a7430aeac2d24878850
Vanshika Sharma <vanshikasharma@192.168.1.16>
2026-05-18 23:29:31 +0530

    logging+exception

---
+++ src/exception.py
@@ -0,0 +1,13 @@
+import sys
+
+def error_message_d(error,error_detail:sys):
+    _,_, exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="error occur [{0}] line number [{1}] msg [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
+
+    return error_message
+
+
+
+
+
